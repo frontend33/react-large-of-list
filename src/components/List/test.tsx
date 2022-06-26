b17e90090ef4fe341fdb59debea5b2882e23db87
georgy.vikulov <georgy.vikulov@aliexpress.ru>
2022-06-27 00:17:28 +0300

    feat: WIP to re render list

--- src/components/List/test.tsx
+++ src/components/List/test.tsx
@@ -3,25 +3,17 @@ import './List.css'
 import {Llist} from "../../utils/LinkedList";
 
 export const List:FC<any> = ({data}) => {
-
-
-    const [updationDebounce, setUpdationDebounce] = useState<any>(0)
-    const [updationInterval, setUpdationInterval] = useState<any>(0)
+    const [nodeArray, setNodeArray] = useState([])
     const [listHeight, setListHeight] = useState<number>(0)
     const [scrollTop, setScrollTop] = useState<number>(0)
     const [heightRow, setHeightRow] = useState<number>(0)
 
     const maxRowRender = Math.floor(listHeight / heightRow)
 
-    const [nodeArray, setNodeArray] = useState<any>([])
-
     const getCalculateVisibleWindow =(top: number, bottom: number, height: number)=> {
         // return (top + listHeight > scrollTop ) && (bottom - (listHeight * 2) - height >= scrollTop)
         // return (top + listHeight > scrollTop )
-
-        // return (top + listHeight > scrollTop )
-        // return (top + listHeight > scrollTop ) && (bottom - height >= scrollTop)
-        return (top <= scrollTop + (listHeight * 1.5) ) && (bottom - (listHeight * -0.5) >= scrollTop)
+        return (top + listHeight > scrollTop ) && (bottom - height >= scrollTop)
     }
 
     const splitArray = useMemo(() => {
@@ -60,45 +52,25 @@ export const List:FC<any> = ({data}) => {
         return nodes
     },[data,listHeight, maxRowRender, scrollTop])
 
-    const containerRef = useRef<HTMLDivElement>(null)
-    const temporaryRef = useRef<HTMLDivElement>(null)
 
-    useEffect(() => {
-        if (maxRowRender) {
-            const dataList = data.slice(0, maxRowRender + 2)
-            setNodeArray([{dataList, isVisible: true}])
-        }
-    },[data, maxRowRender])
-
-    useEffect(() => {
-        if (containerRef && containerRef.current) {
-            const containerHeight = parseInt(String(containerRef.current.clientHeight), 10)
-            setListHeight(containerHeight)
-        }
-
-    },[containerRef])
-
-
-    const getSplitArray = (data: string | any[], listHeight: number, maxRowRender: number, scrollTop: any) => {
+    const getSplitArray = useCallback((data: string | any[], listHeight: number, maxRowRender: number, scrollTop: any) => {
         let nodes = new Llist();
         let subArr: any[] = []
         let count = 0
         let summaryTop = 0
         let summaryBottom = heightRow * data.length
-        let height = (maxRowRender * heightRow) + 10
-        let totalTop = 0
+        let height = 0
 
         for (let i = 0; i < data.length; i++) {
-            // height = i === 0 ? heightRow: heightRow + (heightRow * i)
+            height = i === 0 ? heightRow: heightRow + (heightRow * i)
             if (subArr.length + 1 === maxRowRender) {
-                // let top = i === maxRowRender - 1  ? 0 : summaryTop + listHeight
-                let top = totalTop;
+                // const height = listHeight + heightRow * count
+                let top = i === maxRowRender - 1  ? 0 : summaryTop + listHeight
                 summaryTop = summaryTop + listHeight
                 let bottom = top + height
-                const isVisible = getCalculateVisibleWindow(top, bottom, height)
+                const isVisible = getCalculateVisibleWindow(top, summaryBottom, height)
                 nodes.push({dataList: subArr, height, count: ++count, top, bottom, summaryBottom, isVisible })
                 summaryBottom = summaryBottom - listHeight
-                totalTop += height;
                 subArr = []
             } else {
                 subArr.push(i)
@@ -109,53 +81,33 @@ export const List:FC<any> = ({data}) => {
             const height = listHeight * count + (subArr.length - 1) * heightRow
             const top = summaryTop + listHeight
             let bottom = top + height
-            // const isVisible = getCalculateVisibleWindow(top, summaryBottom, height)
-            const isVisible = getCalculateVisibleWindow(top, bottom, height)
+            const isVisible = getCalculateVisibleWindow(top, summaryBottom, height)
             nodes.push({dataList: subArr, height, top, bottom, summaryBottom, isVisible })
         }
-        return nodes
-    }
+        // @ts-ignore
+        setNodeArray(nodes.toArray())
+        // return nodes
+    },[])
+
+    const containerRef = useRef<HTMLDivElement>(null)
+    const temporaryRef = useRef<HTMLDivElement>(null)
+
+    useEffect(() => {
+        if (containerRef && containerRef.current) {
+            const containerHeight = parseInt(String(containerRef.current.clientHeight), 10)
+            setListHeight(containerHeight)
+        }
+
+    },[containerRef])
+
+    useEffect(() => {
+        getSplitArray(data,listHeight, maxRowRender, scrollTop)
+    }, [getSplitArray, data,listHeight, maxRowRender, scrollTop])
 
     const handleScroll = (event: any) => {
         const scrollTop = event.target.scrollTop;
         setScrollTop(scrollTop)
 
-        clearTimeout(updationDebounce);
-
-        if(!updationInterval) {
-            const intervalUpdateId = setInterval(()=> {
-                // this.updateListVisibility();
-                const dynamic =  getSplitArray(data, listHeight, maxRowRender,scrollTop)
-                setNodeArray(dynamic.toArray())
-            },100);
-
-            setUpdationInterval(intervalUpdateId)
-        }
-
-        let updationTimerId = setTimeout(()=>{
-            // this.updateListVisibility();
-
-            const dynamic =  getSplitArray(data, listHeight, maxRowRender,scrollTop)
-            setNodeArray(dynamic.toArray())
-
-            clearInterval(updationInterval);
-            // updationInterval = null;
-            setUpdationInterval(null)
-        }, 100);
-
-        setUpdationDebounce(updationTimerId)
-
-
-        // const dynamic =  getSplitArray(data, listHeight, maxRowRender,scrollTop)
-        // setNodeArray(dynamic.toArray())
-        //
-        // const test = debounce( () => {
-        //     // const dynamic =  getSplitArray(data, listHeight, maxRowRender,scrollTop)
-        //     // setNodeArray(dynamic.toArray())
-        //     // setScrollTop(scrollTop)
-        //     // console.log('test')
-        // }, 300)
-        // test()
         // console.log('test')
     }
 
@@ -165,11 +117,14 @@ export const List:FC<any> = ({data}) => {
         }
     },[temporaryRef])
 
-    console.log('scrollTop', scrollTop, listHeight, maxRowRender , splitArray, splitArray.toArray())
-    console.log('nodeArray', nodeArray)
+    console.log('scrollTop', scrollTop, listHeight, maxRowRender, splitArray, splitArray.toArray())
+    console.log('tes', nodeArray, !!nodeArray)
+
+    // if (!nodeArray) return null
+
     return (
         <div className="container" ref={containerRef} onScroll={handleScroll}>
-              {/*{data.map((elm: any) => (*/}
+              {/*{list.map((elm: any) => (*/}
               {/*    <div className="element" key={elm}>{elm}</div>*/}
               {/*))}*/}
 
@@ -182,19 +137,19 @@ export const List:FC<any> = ({data}) => {
             </div>
 
             {/*{splitArray.toArray().map((list: any) => (*/}
-            {/*    <>*/}
+            {/*    <div key={list.count}>*/}
             {/*        {list.isVisible && list.dataList.map((elm: any) => (*/}
             {/*            <div className="element" key={elm}>{elm}</div>*/}
             {/*        ))}*/}
-            {/*    </>*/}
+            {/*    </div>*/}
             {/*))}*/}
 
             {nodeArray.map((list: any) => (
-                <>
+                <div key={list.count}>
                     {list.isVisible && list.dataList.map((elm: any) => (
                         <div className="element" key={elm}>{elm}</div>
                     ))}
-                </>
+                </div>
             ))}
         </div>
     );
