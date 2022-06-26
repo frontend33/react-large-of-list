b601bb51935b20da601512334cea519132bc7d60
georgy.vikulov <georgy.vikulov@aliexpress.ru>
2022-06-26 13:22:41 +0300

    feat: WIP to get calculate visible bottom window

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,11 @@
 import React, {useCallback, useMemo} from 'react';
 import { List } from './components/List'
 import './App.css';
-import { Llist } from './utils/LinkedList'
 
 function App() {
-
-    let d = new Llist();
-    console.log(d);
-    d.push(2)
-    d.push(3)
-    d.push(4)
-    console.log(d);
-
     const data = ['0']
 
-    for (let i = 1; i < 300; i++) {
+    for (let i = 1; i < 100; i++) {
         data.push(String(i))
     }
 
--- src/components/List/index.tsx
+++ src/components/List/index.tsx
@@ -2,23 +2,40 @@ import React, {FC, useEffect, useMemo, useRef, useState} from 'react';
 import './List.css'
 import {Llist} from "../../utils/LinkedList";
 
+const heightRow = 60
+
 export const List:FC<any> = ({data}) => {
     const [list, setList] = useState(data)
     const [listHeight, setListHeight] = useState<number>(0)
     const [scrollTop, setScrollTop] = useState<number>(0)
 
-    const maxRowRender = Math.floor(listHeight / 60)
+    const maxRowRender = Math.floor(listHeight / heightRow)
+
+    const getCalculateVisibleWindow =(top: number, bottom: number, height: number)=> {
+        // return top + listHeight > scrollTop work top
+        return top + listHeight > scrollTop
+        // return top + listHeight > scrollTop && (scrollTop + (listHeight * 2)) >= height
+        // return (top <= scrollTop + (listHeight * 1.5) ) && (bottom - (listHeight * -0.5) >= scrollTop)
+    }
 
     const splitArray = useMemo(() => {
-
         let nodes = new Llist();
         let subArr: any[] = []
         let count = 0
 
+        let summaryTop = 0
+        let summaryBottom = heightRow * data.length
+
         for (let i = 0; i < data.length; i++) {
             if (subArr.length + 1 === maxRowRender) {
                 const height = listHeight + listHeight * count
-                nodes.push({dataList: subArr, height, count: ++count})
+                let top = i === maxRowRender - 1  ? 0 : summaryTop + listHeight
+                summaryTop = summaryTop + listHeight
+                // let bottom = top + height
+                const isVisible = getCalculateVisibleWindow(top, summaryBottom, height)
+                // node.bottom = node.top + node.height;
+                nodes.push({dataList: subArr, height, count: ++count, top, bottom: summaryBottom, isVisible })
+                summaryBottom = summaryBottom - listHeight
                 subArr = []
             } else {
                 subArr.push(i)
@@ -26,10 +43,14 @@ export const List:FC<any> = ({data}) => {
         }
 
         if (!!subArr.length) {
-            nodes.push({dataList: subArr, listHeight})
+            const height = listHeight * count + (subArr.length - 1) * heightRow
+            const top = summaryTop + listHeight
+            // let bottom = top + height
+            const isVisible = getCalculateVisibleWindow(top, summaryBottom, height)
+            nodes.push({dataList: subArr, height, top, bottom: summaryBottom, isVisible })
         }
         return nodes
-    },[data,listHeight, maxRowRender])
+    },[data,listHeight, maxRowRender, scrollTop])
 
     const containerRef = useRef<HTMLDivElement>(null)
 
@@ -42,18 +63,21 @@ export const List:FC<any> = ({data}) => {
     },[containerRef])
 
     const handleScroll = (event: any) => {
-        console.log('st__')
         const scrollTop = event.target.scrollTop;
         setScrollTop(scrollTop)
     }
 
-    console.log('scrollTop', scrollTop, listHeight, maxRowRender, splitArray)
+    console.log('scrollTop', scrollTop, listHeight, maxRowRender, splitArray, splitArray.toArray())
 
     return (
         <div className="container" ref={containerRef} onScroll={handleScroll}>
               {list.map((elm: any) => (
                   <div className="element" key={elm}>{elm}</div>
               ))}
+
+            {/*{splitArray.toArray().map((elm: any) => (*/}
+            {/*    <div className="element" key={elm}>{elm.height}</div>*/}
+            {/*))}*/}
         </div>
     );
 }
